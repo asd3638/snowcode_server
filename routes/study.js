6abce2e80be5fcc5d1fb71166180ebd380e69e62
jihye <asd36384@gmail.com>
2021-08-17 20:30:53 +0900

    finish study

--- routes/study.js
+++ routes/study.js
@@ -3,29 +3,66 @@ const { User, Study } = require('../models');
 
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {
-    try {
-        const study = await Study.create({
-            writter: req.body.id,
-            title: req.body.title,
-            content: req.body.content,
-        });
-        console.log(title);
-        res.status(201).json(title);
-        console.log(content);
-        res.status(201).json(content);
-    } catch (err) {
-        console.error(err);
-        next(err);
+router.post('/create', async (req, res, next) => {
+  const { category, title, people, startLine, deadLine, content, wanted, writter } = req.body;
+  try {
+    await Study.create({
+      category,
+      title,
+      startLine,
+      deadLine,
+      content,
+      wanted,
+      people,
+      writter
+    });
+    return res.send("success");
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const study = await Study.findAll();
+    if (study) {
+      res.status(200).send(study);
+    } else {
+      res.status(404).send('no any study');
     }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const study = await Study.findOne({ where: { id: req.params.id } });
+    if (study) {
+      res.status(200).send(study);
+    } else {
+      res.status(404).send('no study');
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 router.route('/:id')
     .patch(async (req, res, next) => {
         try{
-            const result = await Study.updeate({
+            const result = await Study.update({
                 title: req.body.title,
                 content: req.body.content,
+                category: req.body.category,
+                heart: req.body.heart,
+                deadLine: req.body.deadLine,
+                startLine: req.body.startLine,
+                people: req.body.people,
+                writter: req.body.writter
             }, {
                 where: {id: req.params.id},
             });
